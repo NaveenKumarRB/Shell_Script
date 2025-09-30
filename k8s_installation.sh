1400ee26341b0b18c9df04378ab7a4079c2089b2
naveenkumarrb <101310360+NaveenKumarRB@users.noreply.github.com>
2025-09-30 16:26:01 +0530

    Update k8s_installation.sh

--- k8s_installation.sh
+++ k8s_installation.sh
@@ -1,4 +1,4 @@
-__Launching two ubuntu server named masternode (t2. small) and workernode(t2. micro)__
+<ins>__Launching two ubuntu server named masternode (t2. small) and workernode(t2. micro)__</ins>
 
 **<ins>On masternode and workernode</ins>**
 sudo apt-get update
@@ -10,8 +10,18 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 /etc/apt/sources.list.d/kubernetes.list
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list sudo apt-get install -y kubectl kubeadm kubelet
 
-**On masternode**
+**<ins>On masternode</ins>**
 sudo kubeadm init --ignore-preflight-errors=all mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/cal ico.yaml
 kubeadm token create --print-join-command
+
+#[add port 6443 in both master and worker node]
+
+On Worker - >
+#Use kubeadm join command with token (copy the command from master and paste on worker) which you see on 17_line
+
+sudo (command)
+**<ins>On masternode</ins>**
+
+**On Master** - -> kubectl get nodes #to_list_nodes
